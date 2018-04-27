0f9e7c8f07561d968d9fa184d730a7f0665f00cd
ced <zuger.cedric@gmail.com>
2018-04-27 18:27:15 +0200

    Locations processed.

---
+++ work/process_locations.rb
@@ -0,0 +1,41 @@
+require 'pp'
+require 'yaml'
+
+regions = {
+    'Y' => 'Endwith', 'O' => 'Eriador', 'P' => 'Rhuadur', 'DB' => 'Rhoan', 'LB' => 'Gondor',
+    'DG' => 'Mirkwood', 'LG' => 'Misty mountains', 'W' => 'Grey mountains', 'B' => 'Brown lands', 'Red' => 'Mordor'
+}
+
+colors = {
+    'Y' => 'Yellow', 'O' => 'Orange', 'P' => 'Purple', 'DB' => 'Dark blue', 'LB' => 'Light blue',
+    'DG' => 'Dark green', 'LG' => 'Light green', 'W' => 'White', 'B' => 'Brown', 'Red' => 'Red'
+}
+
+perilous_locations = [ 'The Ruins of Angmar', 'Dol Guldur', 'Barad-Dur', 'Moria', 'Mount Gundabad' ]
+heavens = [ 'Fornost', 'Erebor', 'Rivendell', 'Woodland Realm', 'Minas Tirith', 'Lothlorien' ]
+
+def symbolize( name )
+  name.gsub( /[-_ ]/, '_' ).downcase.to_sym
+end
+
+File.open('meq-locref.txt','r') do |f|
+
+  locations = {}
+  location_id = 0
+
+  f.readlines.each do |line|
+    match = line.match /([\w -]+) \((\w+)\)/
+    name = match[1]
+    type = match[2]
+
+    location = { name_code: symbolize( name ), name: name, region: regions[type], region_code: symbolize( regions[type] ),
+                 color: colors[type], color_code: symbolize( colors[type] ),
+                 perilous: perilous_locations.include?( name ), heaven: heavens.include?( name ) }
+    locations[location_id] = location
+    location_id += 1
+  end
+
+  File.open('../app/models/data/locations/locations.yaml','w') do |of|
+    of.puts locations.to_yaml
+  end
+end
