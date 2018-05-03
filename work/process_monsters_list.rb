a0977554f82131752e82a67205f4230d51f180c4
ced <zuger.cedric@gmail.com>
2018-05-03 17:59:22 +0200

    Monsters data.

---
+++ work/process_monsters_list.rb
@@ -0,0 +1,48 @@
+require 'pp'
+require 'yaml'
+
+def symbolize( name )
+  name.gsub( /[-_ ]/, '_' ).downcase.to_sym
+end
+
+cards = {}
+[ :behemoth, :ravager, :zealot ].each do |deck|
+  File.open( "monsters/#{deck}/#{deck}.txt", 'r' ) do |card_file|
+    card_file.readlines.each do |card_line|
+
+      name, type, attack, defense, strength_cost, ability, card_quantity = card_line.split("\t")
+      name_code = symbolize( name )
+      card_quantity = card_quantity.to_i
+
+      cards[deck] ||= { deck: [], type:{} }
+      cards[deck][:type][name_code] = {
+          name: name, type: type, attack: attack.to_i, defense: defense.to_i, strength_cost: strength_cost.to_i }
+      cards[deck][:deck] += Array.new( card_quantity, name_code )
+    end
+  end
+end
+
+monsters = { monsters: {} }
+File.open('monsters/monsters/monsters.txt','r') do |f|
+
+  f.readlines.each do |line|
+    name, fortitude, strength, wisdom, attack_deck = line.split("\t")
+    name_code = symbolize( name )
+    attack_deck = symbolize( attack_deck.chomp )
+
+    monster = { name: name, fortitude: fortitude.to_i, strength: strength.to_i, wisdom: wisdom.to_i, attack_deck: attack_deck }
+    # cards_deck = []
+    #
+    # cards_deck += Array.new( card_quantity, card_id )
+    # card_id += 1
+
+    monsters[:monsters][name_code] = monster
+
+  end
+
+  monsters[:cards] = cards
+
+  File.open('../app/models/data/monsters.yaml','w') do |of|
+    of.puts monsters.to_yaml
+  end
+end
