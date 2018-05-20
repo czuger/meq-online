6d1c177482b4c42617dc0344231e4a72253951d4
ced <zuger.cedric@gmail.com>
2018-05-20 09:12:42 +0200

    StartingPlots class.

--- app/models/game_data/base.rb
+++ app/models/game_data/base.rb
@@ -1,9 +1,20 @@
+require 'yaml'
+require 'ostruct'
 
 module GameData
   class Base
 
+    def initialize
+      p data_file = "#{Rails.root}/app/models/game_data/#{self.class::FILENAME}.yaml"
+      @data = YAML.load_file(data_file)
+    end
+
     def select_tag_data
       @data.map{ |k, v| [ v[:name], k ] }
+    end
+
+    def get( code )
+      OpenStruct.new( @data[code] )
     end
 
   end
--- app/models/game_data/starting_plots.rb
+++ app/models/game_data/starting_plots.rb
@@ -1,5 +1,14 @@
 module GameData
   class StartingPlots < Base
 
+    FILENAME = 'starting_plots'
+
+    def get( code )
+      @data[code][:influence] = OpenStruct.new( @data[code][:influence] ) unless @data[code][:influence].is_a? OpenStruct
+      @data[code][:shadow_cards] = OpenStruct.new( @data[code][:shadow_cards] ) unless @data[code][:shadow_cards].is_a? OpenStruct
+      @data[code][:plot_cards] = OpenStruct.new( @data[code][:plot_cards] ) unless @data[code][:plot_cards].is_a? OpenStruct
+      OpenStruct.new( @data[code] )
+    end
+
   end
 end
