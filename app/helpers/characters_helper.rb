c61478da4b9739679898b3ee2a5c4c6c77d73d01
ced <zuger.cedric@gmail.com>
2018-05-23 11:38:45 +0200

    Character edit working.

--- app/helpers/characters_helper.rb
+++ app/helpers/characters_helper.rb
@@ -3,4 +3,10 @@ module CharactersHelper
   def character_code( char )
     "characters[#{char.name_code}]"
   end
+
+  def local_options_for_select( char )
+    char_location = @board.characters[char.name_code]
+    options_for_select( @locations.alpha_select_tag_data, char_location )
+  end
+
 end
