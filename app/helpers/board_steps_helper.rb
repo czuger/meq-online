f620633a74205a5154859001d4e89623f9f9a677
ced <zuger.cedric@gmail.com>
2018-05-24 10:48:09 +0200

    Step go button disabled when steps list is empty.

--- app/helpers/board_steps_helper.rb
+++ app/helpers/board_steps_helper.rb
@@ -1,2 +1,8 @@
 module BoardStepsHelper
+
+  def events_list
+    @events_list_data ||= @board.aasm.events(:permitted => true).map{ |e| [e.name.to_s.humanize, e.name] }.sort.reverse
+    @events_list_data
+  end
+
 end
