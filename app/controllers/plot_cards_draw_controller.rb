8a2ad8ba63262c6605cd5d25e8cbddd3efb5e7c1
ced <zuger.cedric@gmail.com>
2018-05-25 09:53:54 +0200

    plot cards draw controller operational.

--- app/controllers/plot_cards_draw_controller.rb
+++ app/controllers/plot_cards_draw_controller.rb
@@ -4,52 +4,64 @@ class PlotCardsDrawController < ApplicationController
   before_action :set_actor_ensure_actor, only: [:edit, :update]
 
   def edit
-    @cards = []
-
-    if !@actor.drawn_plot_cards || @actor.drawn_plot_cards.empty?
-      nb_cards = params[:nb_cards].to_i
-      @cards = @board.plot_deck.shift(nb_cards)
-      @actor.drawn_plot_cards= @cards
-
-      @board.transaction do
-
-        @actor.save!
-        @board.save!
-        @board.log!( current_user, @board.sauron, :draw_plot_cards, { count: @cards.count } )
-      end
-    else
-      @cards = @actor.drawn_plot_cards
-    end
+    @cards = @actor.drawn_plot_cards
   end
 
   def update
-    if params[:button] == 'keep'
-      selected_card = JSON.parse(params[:selected_card]).map{ |e| e.to_i }
-
-      raise "Selected cards should not be empty" if selected_card.empty?
-      raise "Selected cards should be included in pool cards" unless (selected_card - @actor.drawn_plot_cards).empty?
-
-      @actor.drawn_plot_cards -= selected_card
-      @actor.plot_cards += selected_card
-
-      @board.transaction do
-        @board.save!
-        @actor.save!
-        @board.log!( current_user, @board.sauron, :keep_plot_card, { count: selected_card.count } )
-      end
+    case params[:button]
+    when 'draw'
+      draw_plot_cards
+    when 'keep'
+      keep_plot_cards
+    when 'put_back'
+      back_to_bottom_of_deck
     else
-      cards_count = @actor.drawn_plot_cards.count
-      @board.plot_deck += @actor.drawn_plot_cards
-      @actor.drawn_plot_cards= []
-
-      @board.transaction do
-        @board.save!
-        @actor.save!
-        @board.log!( current_user, @board.sauron, :cards_bottom_plot_deck, { count: cards_count } )
-      end
+      raise "Unknown order : #{params[:button]}"
     end
 
-    redirect_to @actor
+    redirect_to edit_plot_cards_draw_path(@actor)
+  end
+
+  private
+
+  def back_to_bottom_of_deck
+    cards_count = @actor.drawn_plot_cards.count
+    @board.plot_deck += @actor.drawn_plot_cards
+    @actor.drawn_plot_cards= []
+
+    @board.transaction do
+      @board.save!
+      @actor.save!
+      @board.log!( current_user, @board.sauron, :cards_bottom_plot_deck, { count: cards_count } )
+    end
+  end
+
+  def keep_plot_cards
+    selected_card = JSON.parse(params[:selected_card]).map{ |e| e.to_i }
+
+    raise "Selected cards should not be empty" if selected_card.empty?
+    raise "Selected cards should be included in pool cards" unless (selected_card - @actor.drawn_plot_cards).empty?
+
+    @actor.drawn_plot_cards -= selected_card
+    @actor.plot_cards += selected_card
+
+    @board.transaction do
+      @board.save!
+      @actor.save!
+      @board.log!( current_user, @board.sauron, :keep_plot_card, { count: selected_card.count } )
+    end
+  end
+
+  def draw_plot_cards
+    nb_cards = params[:nb_cards].to_i
+    @cards = @board.plot_deck.shift(nb_cards)
+    @actor.drawn_plot_cards= @cards
+
+    @board.transaction do
+      @actor.save!
+      @board.save!
+      @board.log!( current_user, @board.sauron, :draw_plot_cards, { count: @cards.count } )
+    end
   end
 
 end
