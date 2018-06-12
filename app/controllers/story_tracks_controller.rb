fecbbb2bf854c8df67a4a5972000f35a9f313113
ced <zuger.cedric@gmail.com>
2018-06-12 16:14:44 +0300

    Dominance included in StoryMarker.

--- app/controllers/story_tracks_controller.rb
+++ app/controllers/story_tracks_controller.rb
@@ -4,6 +4,26 @@ class StoryTracksController < ApplicationController
   before_action :set_actor_ensure_board, only: [:edit, :update]
 
   def edit
+    @heroes_to_final = 18 - @board.story_marker_heroes
+
+    sauron_highest_marker = [ @board.story_marker_ring, @board.story_marker_conquest, @board.story_marker_corruption ].max
+    @sauron_to_final = 18 - sauron_highest_marker
+
+    shadowfall_points = 0
+    %w( story_marker_ring story_marker_conquest story_marker_corruption ).each do |sm|
+      shadowfall_points += [ @board.send( sm ), 10 ].min
+    end
+    @sauron_to_shadowfall = 30 - shadowfall_points
+
+    @sauron_dominance = [ @sauron_to_final, @sauron_to_shadowfall ].min
+
+    if @heroes_to_final < @sauron_dominance
+      @dominance = :heroes
+    elsif @heroes_to_final > @sauron_dominance
+      @dominance = :sauron
+    else
+      @dominance = :nobody
+    end
   end
 
   def update
@@ -22,6 +42,7 @@ class StoryTracksController < ApplicationController
                 user_id: current_user.id, actor: @actor)
       end
 
+      redirect_to edit_story_track_path( @actor )
     end
   end
 end
