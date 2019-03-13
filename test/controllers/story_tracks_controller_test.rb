2121e1927b0b278b5fe94533dd2bc42e37be76ff
ced <zuger.cedric@gmail.com>
2019-03-13 10:57:38 +0100

    Added a test for story track controller.

--- test/controllers/story_tracks_controller_test.rb
+++ test/controllers/story_tracks_controller_test.rb
@@ -2,14 +2,28 @@ require 'test_helper'
 
 class StoryTracksControllerTest < ActionDispatch::IntegrationTest
 
-  # test "should get edit" do
-  #   get story_tracks_edit_url
-  #   assert_response :success
-  # end
-  #
-  # test "should get update" do
-  #   get story_tracks_update_url
-  #   assert_response :success
-  # end
+  setup do
+    OmniAuth.config.test_mode = true
+
+    @user = create( :user )
+    @board = create( :board )
+    @sauron = create( :sauron, user: @user, board: @board )
+    @board.users << @user
+
+    $google_auth_hash[:uid] = @user.uid
+    OmniAuth.config.mock_auth[:google_oauth2] = OmniAuth::AuthHash.new    $google_auth_hash
+    get '/auth/google_oauth2'
+    follow_redirect!
+  end
+
+  test 'should get edit' do
+    get edit_story_track_url @sauron
+    assert_response :success
+  end
+
+  test 'should get update' do
+    patch story_track_url @sauron
+    assert_redirected_to edit_story_track_url @sauron
+  end
 
 end
