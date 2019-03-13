a6ccc1a1e9628508a5ca51e58e17cefde3e6e9b5
ced <zuger.cedric@gmail.com>
2019-03-13 10:48:39 +0100

    Added tests for shadow_cards controller.

---
+++ test/controllers/shadow_card_controller_test.rb
@@ -0,0 +1,66 @@
+require 'test_helper'
+
+class ShadowCardControllerTest < ActionDispatch::IntegrationTest
+
+  setup do
+    OmniAuth.config.test_mode = true
+
+    @user = create( :user )
+    @board = create( :board )
+    @board.users << @user
+    @sauron = create( :sauron, board: @board, user: @user )
+
+    $google_auth_hash[:uid] = @user.uid
+    OmniAuth.config.mock_auth[:google_oauth2] = OmniAuth::AuthHash.new    $google_auth_hash
+    get '/auth/google_oauth2'
+    follow_redirect!
+  end
+
+  test 'should get discard_screen' do
+    get plot_cards_discard_screen_url @sauron
+    assert_response :success
+  end
+
+  test 'should get play_screen' do
+    get shadow_cards_play_screen_url @sauron
+    assert_response :success
+  end
+
+  test 'should play card' do
+    @sauron.shadow_cards << 8
+    @sauron.save!
+    post shadow_cards_play_url @sauron, params: { selected_card: 8 }
+    assert_redirected_to sauron_url(@sauron)
+    refute_includes  @sauron.reload.shadow_cards, 8
+  end
+
+  test "should fail because user don't have the card in hand card" do
+    assert_raise do
+      post shadow_cards_play_url @sauron, params: { selected_card: 8, card_slot: 'shadow-card-1' }
+    end
+  end
+
+  test 'should get draw_screen' do
+    get shadow_cards_draw_screen_url @sauron
+    assert_response :success
+  end
+
+  test 'should draw cards' do
+    @sauron.drawn_shadow_cards = []
+    @sauron.save!
+    post shadow_cards_draw_url @sauron, params: { nb_cards: 6 }
+    assert_redirected_to shadow_cards_keep_screen_url(@sauron)
+    assert_equal 6, @sauron.reload.drawn_shadow_cards.count
+  end
+
+  test 'should keep cards' do
+    @sauron.shadow_cards = []
+    @sauron.drawn_shadow_cards = [ 7, 8, 9 ]
+    @sauron.save!
+    post shadow_cards_keep_url @sauron, params: { selected_cards: [ 8, 9 ].join(',' ) }
+    assert_redirected_to shadow_cards_play_screen_url(@sauron)
+    assert_empty  @sauron.reload.drawn_shadow_cards
+    assert_equal [ 8, 9 ], @sauron.reload.shadow_cards
+  end
+
+end
