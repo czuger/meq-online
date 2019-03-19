8881fcda10cb867ade78b9b27afd6959951be1b3
ced <zuger.cedric@gmail.com>
2019-03-19 19:19:11 +0100

    Boards messages tests fixed.

--- test/controllers/board_messages_controller_test.rb
+++ test/controllers/board_messages_controller_test.rb
@@ -10,6 +10,7 @@ class BoardMessagesControllerTest < ActionDispatch::IntegrationTest
     @hero = create( :hero, user: @user, board: @board )
     @sauron = create( :sauron, user: @user, board: @board )
     @board_message = create( :board_message, sender: @sauron, reciever: @hero )
+    @board.users << @user
 
     $google_auth_hash[:uid] = @user.uid
     OmniAuth.config.mock_auth[:google_oauth2] = OmniAuth::AuthHash.new    $google_auth_hash
@@ -17,40 +18,40 @@ class BoardMessagesControllerTest < ActionDispatch::IntegrationTest
     follow_redirect!
   end
 
-  test "should get index" do
-    get board_messages_url
+  test 'should get index' do
+    get actor_board_messages_url(@sauron)
     assert_response :success
   end
   #
-  # test "should get new" do
+  # test 'should get new' do
   #   get new_board_message_url
   #   assert_response :success
   # end
 
-  test "should create board_message" do
+  test 'should create board_message' do
     assert_difference('BoardMessage.count') do
-      post board_messages_url, params: { board_message: { board_id: @board_message.board_id, reciever: @board_message.reciever, sender: @board_message.sender, text: @board_message.text } }
+      post actor_board_messages_url(@sauron), params: { board_message: { reciever_id: @board_message.reciever_id, text: @board_message.text } }
     end
 
-    assert_redirected_to board_message_url(BoardMessage.last)
+    assert_redirected_to actor_board_messages_url(@sauron)
   end
 
-  # test "should show board_message" do
+  # test 'should show board_message' do
   #   get board_message_url(@board_message)
   #   assert_response :success
   # end
 
-  # test "should get edit" do
+  # test 'should get edit' do
   #   get edit_board_message_url(@board_message)
   #   assert_response :success
   # end
   #
-  # test "should update board_message" do
+  # test 'should update board_message' do
   #   patch board_message_url(@board_message), params: { board_message: { board_id: @board_message.board_id, reciever: @board_message.reciever, sender: @board_message.sender, text: @board_message.text } }
   #   assert_redirected_to board_message_url(@board_message)
   # end
 
-  # test "should destroy board_message" do
+  # test 'should destroy board_message' do
   #   assert_difference('BoardMessage.count', -1) do
   #     delete board_message_url(@board_message)
   #   end
