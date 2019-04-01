9969f4234a64c3b6a1950a54a0ec139e584d3db8
ced <zuger.cedric@gmail.com>
2019-04-01 19:59:38 +0200

    Map now show favor, characters and plots location at full size.

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,3 +1,5 @@
+require 'ostruct'
+
 class MapsController < ApplicationController
 
   before_action :require_logged_in
@@ -16,6 +18,23 @@ class MapsController < ApplicationController
     @locations = GameData::Locations.new
 
     @plots = @board.current_plots.order(:plot_position)
+
+    @tokens = {}
+    @board.characters.each do |character, location|
+      @tokens[location] ||= []
+      @tokens[location] << "characters/small_#{character}.jpg".freeze
+    end
+    @board.current_plots.each do |plot|
+      @tokens[plot.affected_location] ||= []
+      @tokens[plot.affected_location] << 'quest_1.png'.freeze
+    end
+    @board.favors.each do |favor_location|
+      @tokens[favor_location] ||= []
+      @tokens[favor_location] << 'favor.png'.freeze
+    end
+
+    pp @tokens
+
   end
 
 end
