196053f1dc9ff3bcf5d678252896e950bb740d74
ced <zuger.cedric@gmail.com>
2019-04-04 15:41:15 +0200

    New board graph.

--- work/game_steps/run.sh
+++ work/game_steps/run.sh
@@ -4,3 +4,9 @@
 dot -Tpng actor.txt > actor.png
 
 dot -Tpng board.txt > board.png
+
+dot -Tpng board2.txt > board2.png
+
+dot -Tpng board2_simplified.txt > board2_simplified.png
+
+dot -Tpng board2_simplified_step_1.txt > board2_simplified_step_1.png
