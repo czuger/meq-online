677e5b244381512144185a89efc8cb32ad868656
ced <zuger.cedric@gmail.com>
2019-04-16 14:37:00 +0200

    Step and Influence controllers removed.

--- app/helpers/board_steps_helper.rb
+++
@@ -1,8 +0,0 @@
-module BoardStepsHelper
-
-  def events_list
-    @events_list_data ||= @board.aasm.events(:permitted => true).map{ |e| [e.name.to_s.humanize, e.name] }.sort.reverse
-    @events_list_data
-  end
-
-end
