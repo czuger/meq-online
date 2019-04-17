3d0dcfd127a46081b5ef5d26c286fbc0a3373916
ced <zuger.cedric@gmail.com>
2019-04-17 08:08:37 +0200

    Mobs and heroes cards reduced.

--- app/models/game_data/heroes.rb
+++ app/models/game_data/heroes.rb
@@ -4,9 +4,9 @@ require 'ostruct'
 module GameData
   class Heroes < Base
 
-    def initialize
-      @data = YAML.load_file("#{Rails.root}/app/models/game_data/heroes.yaml")
-    end
+    # given by :
+    # ( GameData::Heroes.new.common_cards_names + GameData::MobsCards.new.common_cards_names ).inject(Hash.new(0)) { |total, e| total[e] += 1 ;total}.select{ |e, i| i >= 3 }.map{ |e| e.first }.sort
+    KEPT_CARDS = ["Aimed Shot", "Attack of Opportunity", "Charge", "Evade", "Fall Back", "Hack", "Parry", "Ranged Strike", "Reckless", "Smash"]
 
     def get( name_code )
       raise "#{name_code} not found in #{@data.keys}" unless @data.keys.include?(name_code.to_sym)
@@ -22,6 +22,28 @@ module GameData
     def select_heroes_from_board!( board )
       heroes_codes_names_list = board.heroes.map{ |h| h.name_code.to_sym }
       @data.select!{ |k, _| heroes_codes_names_list.include?( k ) }
+    end
+
+    def common_cards_names
+      @data.values.map{ |h| h[:cards].values.map{ |c| c[:name].to_s } }.flatten
+    end
+
+    def kept_cards_per_heroes
+      heroes_hash = Hash[@data.keys.map{ |k| [ k, [] ] }]
+      heroes_hash.keys.each do |hk|
+        @data[hk][:cards].each do |ck, cv|
+          heroes_hash[hk] << ck if KEPT_CARDS.include?( cv[:name].to_s )
+        end
+        cards_backup = heroes_hash[hk]
+        cards = cards_backup.clone
+        cards_pool = []
+        while cards_pool.count < 25
+          cards_pool << cards.shift
+          cards = cards_backup.clone if cards.empty?
+        end
+        heroes_hash[hk] = cards_pool
+      end
+      File.open( 'heroes.yaml', 'w' ){ |f| f.puts heroes_hash.to_yaml }
     end
 
   end
--- app/models/game_data/mobs_cards.rb
+++ app/models/game_data/mobs_cards.rb
@@ -4,6 +4,8 @@ require 'ostruct'
 module GameData
   class MobsCards < Base
 
+    KEPT_CARDS = ["Aimed Shot", "Attack of Opportunity", "Charge", "Evade", "Fall Back", "Hack", "Parry", "Ranged Strike", "Reckless", "Smash"]
+
     def get_deck( attack_deck )
       check_attack_deck(attack_deck)
       @data[attack_deck][:deck].shuffle
@@ -17,6 +19,28 @@ module GameData
     def get_card_pic_path( attack_deck, card_number )
       check_attack_deck(attack_deck)
       @data[attack_deck][:data][card_number.to_i][:pic_path]
+    end
+
+    def common_cards_names
+      @data.values.map{ |h| h[:data].values.map{ |c| c[:name].to_s } }.flatten
+    end
+
+    def kept_cards_per_mob
+      mob_hash = Hash[@data.keys.map{ |k| [ k, [] ] }]
+      mob_hash.keys.each do |hk|
+        @data[hk][:data].each do |ck, cv|
+          mob_hash[hk] << ck if KEPT_CARDS.include?( cv[:name].to_s )
+        end
+        cards_backup = mob_hash[hk]
+        cards = cards_backup.clone
+        cards_pool = []
+        while cards_pool.count < 25
+          cards_pool << cards.shift
+          cards = cards_backup.clone if cards.empty?
+        end
+        mob_hash[hk] = cards_pool
+      end
+      File.open( 'mob_hash.yaml', 'w' ){ |f| f.puts mob_hash.to_yaml }
     end
 
     private
