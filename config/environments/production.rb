a82506b7c527c426c1952c4658be422740e37a55
ced <zuger.cedric@gmail.com>
2019-05-05 10:02:49 +0200

    Action cable configuration.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_cable.url = 'wss://meq-online.ieroe.com/cable' # ws:// is non-secure, wss:// is secure
+  config.action_cable.allowed_request_origins = [ 'https://meq-online.ieroe.com' ]
 end
