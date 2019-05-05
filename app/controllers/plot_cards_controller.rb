377981fca0a144dbc2c2deff2a2cba9949033134
ced <zuger.cedric@gmail.com>
2019-05-05 11:53:39 +0200

    Only cards that meets requirements could be played.

--- app/controllers/plot_cards_controller.rb
+++ app/controllers/plot_cards_controller.rb
@@ -39,6 +39,7 @@ class PlotCardsController < ApplicationController
     game_data = GameData::Plots.new
 
     @playable_cards = @actor.plot_cards.to_a.select{ |e| game_data.requirement( @board,e ) }
+    @unplayable_cards = @actor.plot_cards - @playable_cards
 
     @free_slots = 1.upto(3).to_a - @board.current_plots.map{ |e| e.plot_position }
 
